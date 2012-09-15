6bdc976ce463ebf33b81815aa524a4d267e16103
Nicholas Sherlock <n.sherlock@gmail.com>
2012-09-15 04:08:31 +0000

    Couple of updates to the JNLP stubs

--- src/javax/jnlp/FileContents.java
+++ src/javax/jnlp/FileContents.java
@@ -6,4 +6,6 @@ public interface FileContents {
 
 	public InputStream getInputStream();
 
+	public String getName();
+	
 }
--- src/javax/jnlp/FileSaveService.java
+++ src/javax/jnlp/FileSaveService.java
@@ -6,4 +6,5 @@ public interface FileSaveService {
 
 	public FileContents saveFileDialog(java.lang.String arg0, java.lang.String[] arg1, java.io.InputStream arg2, java.lang.String arg3) throws IOException;
 
+	FileContents saveAsFileDialog(java.lang.String pathHint, java.lang.String[] extensions, FileContents contents);
 }
