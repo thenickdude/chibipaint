7f94503e2d6438596e22ba277f2cd2dcb849f05f
Nicholas Sherlock <n.sherlock@gmail.com>
2012-09-25 02:42:18 +0000

    Use 16KB chunks for upload

--- src/chibipaint/gui/CPSendDialog.java
+++ src/chibipaint/gui/CPSendDialog.java
@@ -10,8 +10,6 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -192,7 +190,7 @@ public class CPSendDialog extends JDialog implements ActionListener {
 						 * Write the data in chunks so we can give a progress
 						 * bar
 						 */
-						final int CHUNK_SIZE = haveProgress ? 8 * 1024 : 64 * 1024;
+						final int CHUNK_SIZE = haveProgress ? 16 * 1024 : 64 * 1024;
 						byte[] chunk = new byte[CHUNK_SIZE];
 						int bytesRead, progress = 0;
 						
